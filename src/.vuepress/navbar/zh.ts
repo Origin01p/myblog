22cedd5e634da938f225fcca42fb58770c3a907e
Origin01p <origin01p@outlook.com>
2024-05-12 00:27:16 +0800

    Update zh.ts

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -4,7 +4,7 @@ export const zhNavbar = navbar([
   "/",
   { text: "导航", icon: "navigation", link: "/posts/quicknav/" },
   //{ text: "博客主页", icon: "blog", link: "/posts/blog/" },
-  { text: "笔记归档", icon: "docs", link: "/posts/codenotes/" },
+  { text: "笔记归档", icon: "inbox", link: "/posts/codenotes/" },
   { text: "浮生杂记", icon: "teacup", link: "/posts/floatinglife/" },
   //{ text: "开源项目", icon: "free", link: "/posts/projects/" },
   {
