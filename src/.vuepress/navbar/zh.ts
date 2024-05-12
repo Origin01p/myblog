f760c93bb637d06feb44f6bb30a03b73a7085e1b
Origin01p <origin01p@outlook.com>
2024-05-12 20:02:44 +0800

    1

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -6,7 +6,6 @@ export const zhNavbar = navbar([
   //{ text: "博客主页", icon: "blog", link: "/posts/blog/" },
   { text: "笔记归档", icon: "pencil", link: "/posts/notes/" },
   { text: "浮生杂记", icon: "mug-saucer", link: "/posts/floatinglife/" },
-  //{ text: "开源项目", icon: "free", link: "/posts/projects/" },
   {
     text: "安娜档案",
     icon: "inbox",
@@ -15,12 +14,12 @@ export const zhNavbar = navbar([
       {
         text: "书籍资源",
         icon: "book",
-        link: "/posts/floatinglife/resources/books/README.md",
+        link: "/posts/resources/books/README.md",
       },
       {
         text: "影音资源",
         icon: "play",
-        link: "/posts/floatinglife/resources/videos/",
+        link: "/posts/resources/videos/",
       },
     ],
   },
--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -14,46 +14,7 @@ export const zhSidebar = sidebar({
           icon: "write",
           link: "基础-面向对象.md",
         },
-        {
-          text: "Java基础-泛型机制",
-          icon: "write",
-          link: "Java基础-泛型机制.md",
-        },
-        {
-          text: "Java基础-注解机制",
-          icon: "write",
-          link: "Java基础-注解机制.md",
-        },
-        {
-          text: "Java基础-异常机制",
-          icon: "write",
-          link: "Java基础-异常机制.md",
-        },
-        {
-          text: "Java基础-反射机制",
-          icon: "write",
-          link: "Java基础-反射机制.md",
-        },
-        {
-          text: "Java集合-类关系图",
-          icon: "write",
-          link: "Java集合-类关系图.md",
-        },
-        {
-          text: "Java集合-ArrayList",
-          icon: "write",
-          link: "Java集合-ArrayList.md",
-        },
-        {
-          text: "Java8新特性",
-          icon: "write",
-          link: "Java8新特性.md",
-        },
-        {
-          text: "Java中的SPI机制",
-          icon: "write",
-          link: "Java中的SPI机制.md",
-        },
+        
       ],
     },
     {
